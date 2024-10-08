7cf566e7f65b6ddd04eb4ba49ab6685c70c90ba3
unknown <eyad.shabaaan@gmail.com>
2024-10-08 22:02:20 +0400

    first commit

---
+++ components/Feed.jsx
@@ -0,0 +1,69 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleTagClick = (tag) => {
+    setSearchText(tag);
+  };
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const filteredPosts = posts.filter(
+    (post) =>
+      post.creator.username.toLowerCase().includes(searchText.toLowerCase()) ||
+      post.prompt.toLowerCase().includes(searchText.toLowerCase()) ||
+      post.tag.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList
+        data={searchText ? filteredPosts : posts}
+        handleTagClick={handleTagClick}
+      />
+    </section>
+  );
+};
+
+export default Feed;
